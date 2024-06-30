c38204354bcdf88396e5a4a597859e9fb19d103b
Petro Atamaniuk <dark.pristav@gmail.com>
2024-06-30 15:52:26 +0200

    5.4.finish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const inputRef = useRef(null)
+
+  const handleClick = (event) => {
+    event.preventDefault()
+    console.log(inputRef.current.value)
+    inputRef.current.value = "" 
+  }
+
+  return <div>
+    <form>
+      <input ref={inputRef} type="text" />
+      <button onClick={handleClick}>Vypsat a vyčistit</button>
+    </form>
+  </div>
 }
 
-export default App;
+export default App
