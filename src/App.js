e925d26b190037ad3f1a1c686e582dc90db11ad1
Petro Atamaniuk <dark.pristav@gmail.com>
2024-06-30 15:48:35 +0200

    5.1.final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [firstName, setFirstName] = useState("")
+  const [names, setNames] = useState([])
+
+  const formSubmit = (event) => {
+    event.preventDefault()
+
+    if (firstName) {
+      setNames((names) => {
+        return [...names, firstName]
+      })
+  } else {
+    alert("Nebylo nic vyplněno")
+  }
+
+    setFirstName("")
+  }  
+
+  return <article>
+    <form onSubmit={formSubmit}>
+      <input
+        className="userName"
+        type="text"
+        placeholder="Jméno"
+        value={firstName}
+        onChange={(event) => setFirstName(event.target.value)}
+      />
+      <button>Odeslat</button>
+    </form>
+   {names.map((oneName, index) => {
+     return <p
+       className="oneName"
+       key={index}>
+       {oneName}
+     </p>
+})}
+  </article>
+
 }
 
-export default App;
+export default App
